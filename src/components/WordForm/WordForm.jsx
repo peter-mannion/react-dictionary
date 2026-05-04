b6086211cc6f68fbdf9ea40d842048057046a9b9
P M <pvmannion@gmail.com>
2026-05-04 14:55:09 -0700

    Add handleSubmit event handler

--- src/components/WordForm/WordForm.jsx
+++ src/components/WordForm/WordForm.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./WordForm.css";
 
-export default function WordForm() {
+const WordForm = () => {
+  const hanldeSubmit = (e) => {
+    e.preventDefault();
+
+    const enField = document.querySelector("[name=en]");
+    const deField = document.querySelector("[name=de]");
+
+    const enValue = enField.value;
+    const deValue = deField.value;
+
+    enField.value = "";
+    deField.value = "";
+
+    alert(`${enValue} - ${deValue}`);
+  };
+
   return (
     <section className="card-form">
       <h2>New Card</h2>
-      <form action="#" method="GET">
+      <form action="#" method="GET" onSubmit={hanldeSubmit}>
         <div className="form-row">
           <label>
             English:
@@ -24,4 +39,6 @@ export default function WordForm() {
       </form>
     </section>
   );
-}
+};
+
+export default WordForm;
